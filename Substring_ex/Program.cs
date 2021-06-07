4a4858a29bd48573dcc18c17fd2d1d15615ae499
Soumay kanti Das <anikds2859@outlook.com>
2021-06-07 15:32:50 +0600

    Add project files.

---
+++ Substring_ex/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Substring_ex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "hellow world";
+            string s1 = "orange";
+            string s2 = "red";
+            s1 += s2;
+            Console.WriteLine(s1);
+            s1 = s1.Substring(2, 5);
+            Console.WriteLine(s1);
+
+        }
+    }
+}
